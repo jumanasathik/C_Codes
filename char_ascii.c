bd77bb40a5470cb32819c8021d2d99369df0709b
Jumana Sathik <jumanasathik4002@gmail.com>
2026-02-03 18:26:59 +0530

    Program to display character and its ASCII value
    This program accepts a single character from the user and prints it back to confirm input. It then prints the internal ASCII numeric value of that character using type casting rules of C.

---
+++ char_ascii.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    char alpha;
+    printf("Enter a character: ");
+    scanf("%c", &alpha);
+    printf("You entered %c.\n", alpha);
+    printf("ASCII value is %d.\n", alpha);
+    return 0;
+}
